7a3d65e5f1e3afb47bde59e6a5ce94651011a321
Srikanthdubba <srikanthdubba90@gmail.com>
2025-09-01 15:38:45 +0530

    Add files via upload

---
+++ circularQ.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#define MAX 4
+
+int queue[MAX];
+int front = -1, rear = -1;
+
+int isFull() {
+    return ((front == 0 && rear == MAX - 1) || (rear + 1) % MAX == front);
+}
+
+int isEmpty() {
+    return front == -1;
+}
+
+void insert() {
+    int num;
+    if (isFull()) {
+        printf("\nOVERFLOW");
+        return;
+    }
+    printf("\nEnter the number to be inserted in the queue: ");
+    scanf("%d", &num);
+
+    if (isEmpty())
+        front = rear = 0;
+    else
+        rear = (rear + 1) % MAX;
+
+    queue[rear] = num;
+}
+
+int delete_element() {
+    if (isEmpty()) {
+        printf("\nUNDERFLOW");
+        return -1;
+    }
+
+    int val = queue[front];
+    if (front == rear)
+        front = rear = -1;
+    else
+        front = (front + 1) % MAX;
+
+    return val;
+}
+
+int peek() {
+    if (isEmpty()) {
+        printf("\nQUEUE IS EMPTY");
+        return -1;
+    }
+    return queue[front];
+}
+
+void display() {
+    if (isEmpty()) {
+        printf("\nQUEUE IS EMPTY");
+        return;
+    }
+
+    printf("\nQueue elements are: ");
+    int i = front;
+    while (1) {
+        printf("%d ", queue[i]);
+        if (i == rear)
+            break;
+        i = (i + 1) % MAX;
+    }
+}
+
+int main() {
+    int option, val;
+
+    do {
+        printf("\n\n***** MAIN MENU *****");
+        printf("\n1. Insert an element");
+        printf("\n2. Delete an element");
+        printf("\n3. Peek");
+        printf("\n4. Display the queue");
+        printf("\n5. EXIT");
+        printf("\nEnter your option: ");
+        scanf("%d", &option);
+
+        switch (option) {
+            case 1:
+                insert();
+                break;
+            case 2:
+                val = delete_element();
+                if (val != -1)
+                    printf("\nThe number deleted is: %d", val);
+                break;
+            case 3:
+                val = peek();
+                if (val != -1)
+                    printf("\nThe first value in queue is: %d", val);
+                break;
+            case 4:
+                display();
+                break;
+            case 5:
+                printf("\nExiting...");
+                break;
+            default:
+                printf("\nInvalid option! Try again.");
+        }
+    } while (option != 5);
+
+    return 0;
+}
+
